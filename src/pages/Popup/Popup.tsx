5852cf02212086a408f7eb134c867fce1039d05f
Prish20 <contact@christadriansanya.me>
2025-02-16 04:32:29 +0300

    refactor: rename project to AdFriend, update version, and modify popup layout

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+
 import './Popup.css';
 
 const Popup = () => {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>Hello Welcome to Adfriend</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React!
-                </a>
-            </header>
-            <Greetings />
+            <h1 className='nametext'>AdFriend</h1>
         </div>
     );
 };
